7ca98a0ab12434efe5cb1ba4a982856d6cc0dab5
Blaise Larrabee <blaiselarrabee@gmail.com>
2021-04-26 23:04:34 +0000

    checking to see if user has metamask installed

--- swap-wallet/start/contract.js
+++ swap-wallet/start/contract.js
@@ -42,5 +42,33 @@ const initialize = () => {
     const { ethereum } = window;
     return Boolean(ethereum && ethereum.isMetaMask);
   };
+
+  //------Inserted Code------\\
+  const MetaMaskClientCheck = () => {
+  //Now we check to see if Metmask is installed
+  if (!isMetaMaskInstalled()) {
+    //If it isn't installed we ask the user to click to install it
+    onboardButton.innerText = 'Click here to install MetaMask!';
+    //When the button is clicked we call this function
+    onboardButton.onclick = onClickInstall;
+    //The button is now disabled
+    onboardButton.disabled = false;
+  } else {
+    //If it is installed we change our button text
+    onboardButton.innerText = 'Connect';
+  }
+};
+//We create a new MetaMask onboarding object to use in our app
+const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+
+//This will start the onboarding proccess
+const onClickInstall = () => {
+  onboardButton.innerText = 'Onboarding in progress';
+  onboardButton.disabled = true;
+  //On this object we have startOnboarding which will start the onboarding process for our end user
+  onboarding.startOnboarding();
+};
+    MetaMaskClientCheck();
+  };
 }
 window.addEventListener('DOMContentLoaded', initialize)
