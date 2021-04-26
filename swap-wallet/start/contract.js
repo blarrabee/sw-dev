8b28b03fc29f718a4aa8ba0ed30dabfb658bc7da
Blaise Larrabee <blaiselarrabee@gmail.com>
2021-04-26 22:57:19 +0000

    Added onboarding button for metamask.

--- swap-wallet/start/contract.js
+++ swap-wallet/start/contract.js
@@ -33,6 +33,14 @@ The `piggybankContract` is compiled from:
 const forwarderOrigin = 'http://localhost:9010'
 
 const initialize = () => {
-  //You will start here 
+  //Basic Actions Section
+  const onboardButton = document.getElementById('connectButton');
+
+  //Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
 }
 window.addEventListener('DOMContentLoaded', initialize)
